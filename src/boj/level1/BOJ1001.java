cbf51072bab2ed840db096a10cb9fc16f6c47d11
who-hoo <who.ho3ov@gmail.com>
2021-11-06 23:26:56 +0900

    Feat: BOJ 1001 A-B

---
+++ src/boj/level1/BOJ1001.java
@@ -0,0 +1,15 @@
+package boj.level1;
+
+import java.util.*;
+
+public class BOJ1001 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        System.out.println(a - b);
+    }
+}
