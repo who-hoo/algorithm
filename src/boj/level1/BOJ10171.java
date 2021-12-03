f9fde67b2b5a61bf7723ef69c19c61c390a8befb
who-hoo <who.ho3ov@gmail.com>
2021-12-04 00:33:09 +0900

    Feat: BOJ 10171 고양이

---
+++ src/boj/level1/BOJ10171.java
@@ -0,0 +1,11 @@
+package boj.level1;
+
+public class BOJ10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
