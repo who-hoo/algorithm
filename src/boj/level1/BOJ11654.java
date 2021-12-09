200e2b2324d5cad32a706acb99c0c9afef69b92f
who-hoo <who.ho3ov@gmail.com>
2021-12-09 23:24:49 +0900

    Feat: BOJ 11654 아스키 코드

---
+++ src/boj/level1/BOJ11654.java
@@ -0,0 +1,14 @@
+package boj.level1;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
+
+        System.out.print((int) input.charAt(0));
+    }
+}
