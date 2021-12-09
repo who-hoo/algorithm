efc0ecb4c07bd59997f118d643bfef09fa4d85cc
who-hoo <who.ho3ov@gmail.com>
2021-12-09 23:33:49 +0900

    Feat: BOJ 1008 A/B

---
+++ src/boj/level1/BOJ1008.java
@@ -0,0 +1,14 @@
+package boj.level1;
+
+import java.util.Scanner;
+
+public class BOJ1008 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        System.out.println((double) a / b);
+    }
+}
