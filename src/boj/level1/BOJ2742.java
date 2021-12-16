c4a082e3a482beea650144caa1d2c568a63b0df7
who-hoo <who.ho3ov@gmail.com>
2021-12-16 13:58:18 +0900

    Feat: BOJ 2742 기찍 N

---
+++ src/boj/level1/BOJ2742.java
@@ -0,0 +1,16 @@
+package boj.level1;
+
+import java.util.Scanner;
+
+public class BOJ2742 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
