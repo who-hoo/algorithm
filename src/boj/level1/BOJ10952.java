8c9172376548f8a3439292da17e21e9597f1b9f9
who-hoo <who.ho3ov@gmail.com>
2021-12-20 10:53:53 +0900

    Feat: BOJ 10952 A+B - 5

---
+++ src/boj/level1/BOJ10952.java
@@ -0,0 +1,20 @@
+package boj.level1;
+
+import java.util.Scanner;
+
+public class BOJ10952 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        while (true) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            System.out.println(a + b);
+        }
+        sc.close();
+    }
+}
