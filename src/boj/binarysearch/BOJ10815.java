6fe63c4af4c7c60a3b874205fedd86abba3d1c7b
who-hoo <who.ho3ov@gmail.com>
2022-03-10 01:52:54 +0900

    :sparkles: BOJ 10815 숫자 카드

---
+++ src/boj/binarysearch/BOJ10815.java
@@ -0,0 +1,68 @@
+package boj.binarysearch;
+
+/*
+	문제    : BOJ 숫자 카드
+    유형    : 정렬, 이분 탐색
+	난이도   : Easy (Silver4)
+	시간    : 1h
+	uri    : https://www.acmicpc.net/problem/10815
+    날짜    : 22.03.09(o)
+    refer  :
+*/
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ10815 {
+
+    static int[] nArr;
+
+    static int binarySearch(int targetL, int targetR, int target) {
+        while (targetL <= targetR) {
+            int mid = (targetL + targetR) / 2;
+            if (nArr[mid] == target) {
+                return 1;
+            }
+            if (target < nArr[mid]) {
+                targetR = mid - 1;
+            } else {
+                targetL = mid + 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        nArr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nArr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nArr);
+
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            int result = binarySearch(0, n - 1, target);
+            sb.append(result).append(" ");
+        }
+        br.close();
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
