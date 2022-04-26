751566537693f7d4558e8d0754ca0374d3adcc1c
who-hoo <who.ho3ov@gmail.com>
2022-04-26 22:08:34 +0900

    :poop: 프로그래머스 60057 문자열 압축(오답)

---
+++ src/programmers/level2/Programmers60057.java
@@ -0,0 +1,69 @@
+package programmers.level2;
+
+public class Programmers60057 {
+
+    public String compress(String s, int unit) {
+        StringBuilder result = new StringBuilder();
+        int cnt = 1;
+        String prev = s.substring(0, unit);
+        for (int i = unit; i < s.length(); i += unit) {
+            try {
+                String current = s.substring(i, i + unit);
+                if (current.equals(prev)) {
+                    cnt++;
+                } else {
+                    if (cnt == 1) {
+                        result.append(prev);
+                    } else {
+                        result.append(cnt).append(prev);
+                    }
+                    cnt = 1;
+                    prev = current;
+                }
+            } catch (IndexOutOfBoundsException e) {
+                result.append(s.substring(i));
+                cnt = 1;
+                prev = "";
+            }
+        }
+        if (cnt == 1) {
+            result.append(prev);
+        } else {
+            result.append(cnt).append(prev);
+        }
+        if (s.length() % unit > 0) {
+            result.append(s.substring(s.length() - unit));
+        }
+        return result.toString();
+    }
+
+    public int solution(String s) {
+        int min = s.length();
+        for (int i = 1; i <= s.length() / 2; i++) {
+            String compressed = compress(s, i);
+            min = Math.min(min, compressed.length());
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Programmers60057 sol = new Programmers60057();
+        String[] inputs = new String[]{
+            "aabbaccc",
+            "ababcdcdababcdcd",
+            "abcabcdede",
+            "abcabcabcabcdededededede",
+            "xababcdcdababcdcd",
+            "aaaaaaaaaaaabcd",
+            "xxxxxxxxxxyyy",
+            "aababa",
+            "a",
+            "abcabcdededededede",
+            "xztjabcdabcd",
+            "acdhdh"
+        };
+        for (String input : inputs) {
+            System.out.println(sol.solution(input));
+        }
+    }
+}
