be523e72c931ae9e11215e626463428cb336fd4f
who-hoo <who.ho3ov@gmail.com>
2022-07-20 17:31:29 +0900

    :sparkles: BOJ 5639 이진 검색 트리

--- src/boj/level4/BOJ5639.java
+++ src/boj/level4/BOJ5639.java
@@ -1,15 +1,19 @@
 package boj.level4;
 
-import com.sun.source.tree.BinaryTree;
+/*
+    문제    : BOJ 이진 검색 트리
+    유형    : 그래프 이론, 그래프 탐색, 트리, 재귀
+	난이도   : Hard (Silver5)
+	시간    : 3h
+	uri    : https://www.acmicpc.net/problem/5639
+    refer  :
+*/
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
 
 public class BOJ5639 {
 
@@ -19,10 +23,33 @@ public class BOJ5639 {
 		Node left;
 		Node right;
 
-		Node(int n, Node left, Node right) {
+		Node(int n) {
 			this.n = n;
-			this.left = left;
-			this.right = right;
+		}
+
+		void insert(Node node) {
+			if (this.n > node.n) {
+				if (this.left == null) {
+					this.left = node;
+				} else {
+					this.left.insert(node);
+				}
+			} else {
+				if (this.right == null) {
+					this.right = node;
+				} else {
+					this.right.insert(node);
+				}
+			}
+		}
+	}
+
+	static class BinaryTree {
+
+		Node root;
+
+		BinaryTree(Node root) {
+			this.root = root;
 		}
 	}
 
@@ -30,12 +57,26 @@ public class BOJ5639 {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		List<Integer> nodes = new ArrayList<>();
+		Node root = new Node(Integer.parseInt(in.readLine()));
+		BinaryTree tree = new BinaryTree(root);
 		String line;
 		while ((line = in.readLine()) != null) {
-			nodes.add(Integer.parseInt(line));
+			Node node = new Node(Integer.parseInt(line));
+			tree.root.insert(node);
 		}
+		postOrder(out, root);
 
-		System.out.println(nodes);
+		out.close();
+		in.close();
+	}
+
+	private static void postOrder(BufferedWriter out, Node node) throws IOException {
+		if (node.left != null) {
+			postOrder(out, node.left);
+		}
+		if (node.right != null) {
+			postOrder(out, node.right);
+		}
+		out.write(node.n + System.lineSeparator());
 	}
 }
